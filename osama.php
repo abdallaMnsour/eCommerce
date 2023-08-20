7e60b44ee4735d2a52f276ff7b84987823cceac7
bodegg <bodemansour8@gmail.com>
2023-08-21 01:41:13 +0300

    move bugs :)

--- osama.php
+++ osama.php
@@ -1,6 +1,6 @@
 <?php
 
 $array = [1,1,3,5];
-foreach ($array as key => value) :
-  echo "this is key" . $key . "this is falue" . $value;
+foreach ($array as $key => $value) :
+  echo "this is key" . $key . "this is value" . $value;
 endforeach;
